91722c04731f6a71f9a3fea5515bfc79f3113000
OBJULL <accounts@objnull.net>
2024-03-10 17:18:28 -0400

    Adding arguments

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,9 @@
 #include <interface/cli.h>
 
 // Functions
-
-// Entry point
-int main(int argc, char* argv[]){
+void cli_version(){
     // Loading a new test
-    Quiz quiz = Quiz("data/exams/CompTIA 1101.json", 50);
+    Quiz quiz = Quiz("data/exams/CompTIA 1101.json", 5);
 
     // Init application
     Tester_CLI cli;
@@ -18,17 +16,19 @@ int main(int argc, char* argv[]){
     std::string menu; std::cin >> menu;
     int numOfQuestions = 0;
 
-    if(menu == "1"){
+    // What page do we want?
+    switch(menu[0]){
+    case '1':
         numOfQuestions = 15;
-    }
-    else if(menu == "2"){
+        break;
+    case '2':
         numOfQuestions = 30;
-    }
-    else if(menu == "3"){
+        break;
+    case '3':
         numOfQuestions = 60;
-    }
-    else if(menu == "4"){
-        return 0;
+        break;
+    case '4':
+        return;
     }
 
     // Making new quiz
@@ -36,6 +36,31 @@ int main(int argc, char* argv[]){
 
     // Asking the user questions
     cli.AskQuestions(quiz, numOfQuestions);
+}
+
+// Entry point
+int main(int argc, char* argv[]){
+    // Getting argument(s)
+    std::vector<std::string> args;
+    for(unsigned int i = 0; i < argc; i++){
+        args.push_back(std::string(argv[i]));
+    }
+
+    // Checking the arguments
+    if(args[1] == "--help"){
+        printf("Usage: TestDesk [OPTIONS]\n");
+        printf("OPTIONS: \n");
+        printf("\t--help: Displays how to use the program\n");
+        printf("\t--cli: Starts program in a Command Line Interface.\n");
+        return 0;
+    }
+    else if(args[1] == "--cli"){
+        cli_version();
+        return 0;
+    }
+
+    // Starting the GUI version
+    
 
     // Exit
     return 0;
